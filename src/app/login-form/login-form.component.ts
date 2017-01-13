ac956337a7d01bf066abc78274cbbfc9c340e4fa
alexander.sokolov <alexandersokolov17@gmail.com>
2017-01-13 13:57:40 -0400

    Update

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-//Imports the userLogin class that makes it easier to store the user data
-import { userLogin } from './userLogin/userLogin';
+
 //Imports the http class that allows us to make http requests
 import {Http, Response, Headers} from '@angular/http';
+
 
 @Component({
   selector: 'app-login-form',
@@ -17,11 +17,10 @@ export class LoginFormComponent implements OnInit
   userError = "";
   passError = "";
 
-  //User Class that stores information provided by the user
-  userInfo: userLogin;
 
-  //JSON data that will be sent
-  JData: string;
+  logInfo = {"username": "", "password": ""};
+
+
 
   //JSON data that will be received
   RData: Object;
@@ -29,7 +28,6 @@ export class LoginFormComponent implements OnInit
 
   constructor(private http: Http)
   {
-    this.userInfo = new userLogin;
   }
 
   ngOnInit()
@@ -40,8 +38,8 @@ export class LoginFormComponent implements OnInit
   onSubmit(form: any): void
   {
     //console.log('you submitted value:', form);
-    console.log('Username: '+ form.username);
-    console.log('Password: ' + form.password);
+    //Delete this later
+    console.log("Message: " + JSON.stringify(this.logInfo));
 
 
 
@@ -65,18 +63,8 @@ export class LoginFormComponent implements OnInit
     }
 
 
-    //This is activated if authentication was not successful
-    //this.validated = false;
-
     if(form.username != "" && form.password != "")
     {
-
-      this.userInfo.username = form.username;
-      this.userInfo.password = form.password;
-
-      //Turns userInfor object into json
-      this.JData = JSON.stringify(this.userInfo);
-
 
       //Http request test (GET)
       /*
@@ -93,7 +81,7 @@ export class LoginFormComponent implements OnInit
       headers.append('Content-Type', 'application/json');
 
       this.http
-        .post('http://localhost:3000/authenticate', this.JData, {headers: headers})
+        .post('http://localhost:3000/authenticate', JSON.stringify(this.logInfo), {headers: headers})
         .subscribe(data => {  if(JSON.parse(data["_body"])[0]["value"] == "true"){this.validated=true;alert("Login success");} else{this.validated=false} }
         ,error => {console.log(JSON.stringify(error.json()));});
 
--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-//Imports the userLogin class that makes it easier to store the user data
-import { registrationInfo } from './registrationInfo/registrationInfo';
-
 //Imports the http class that allows us to make http requests
 import {Http, Response, Headers} from '@angular/http';
-import {isUndefined} from "util";
 
 @Component({
   selector: 'app-registration-form',
@@ -22,18 +18,11 @@ export class RegistrationFormComponent implements OnInit
   passError = "";
 
   //RegistrationInfo Class that stores information provided by the user
-  regInfo: registrationInfo;
-
-  //JSON data that will be sent
-  JData: string;
-
-  //JSON data that will be received
-  RData: Object;
+  regInfo = {"username": "", "password": ""};
 
 
   constructor(private http: Http)
   {
-    this.regInfo = new registrationInfo;
   }
 
   ngOnInit()
@@ -43,8 +32,7 @@ export class RegistrationFormComponent implements OnInit
   onSubmit(form: any): void
   {
     //console.log('you submitted value:', form);
-    console.log('Username: '+ form.username);
-    console.log('Password: ' + form.password);
+    console.log("Message: " + JSON.stringify(this.regInfo));
 
 
 
@@ -74,37 +62,17 @@ export class RegistrationFormComponent implements OnInit
     if(form.username != "" && form.password != "")
     {
 
-      this.regInfo.username = form.username;
-      this.regInfo.password = form.password;
-
-      //Turns userInfor object into json
-      this.JData = JSON.stringify(this.regInfo);
-
-
-      //Http request test (GET)
-      /*
-       this.http.request('http://localhost:3000/')
-       .subscribe((res: Response) => {
-       this.RData = res.json();
-       console.log(res.json());
-       });
-       */
-      //POST REQUEST
-
       //'Content-Type: application/json'
       var headers = new Headers();
       headers.append('Content-Type', 'application/json');
 
       this.http
-        .post('http://localhost:3000/users', this.JData, {headers: headers})
+        .post('http://localhost:3000/users', JSON.stringify(this.regInfo), {headers: headers})
         .subscribe(data => {  /* Does nothing right now */ }
           ,error => {console.log(JSON.stringify(error.json()));});
 
 
       alert("Registration is successful");
-
-      //To get the validation value: JSON.parse(data["_body"])[0]["value"]
-
 
 
     }
